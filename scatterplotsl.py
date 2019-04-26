c64decb9851ba975171670497fb61001b7376aaa
chloemcgarry <47299403+chloemcgarry@users.noreply.github.com>
2019-04-27 00:59:39 +0100

    Scatter plot for Sepal Length

---
+++ scatterplotsl.py
@@ -0,0 +1,18 @@
+#Importing pandas library
+import pandas as pd 
+#Importing matplolib library
+import matplotlib.pyplot as plt 
+
+#Read csv file and call it data
+data = pd.read_csv('iris_data_set.csv')
+
+#labelling x axis and y axis
+x = data.species
+y = data.sepal_length
+
+plt.scatter(x,y)
+
+#Naming the scatter plot of the sepal length data
+title = ("Sepal Length Comparison Plot")
+#Plotting the sepal length data on a scatter plot
+plt.show
