e81da233f5ddce513fe5cd6ffec049ea055ae117
chloemcgarry <47299403+chloemcgarry@users.noreply.github.com>
2019-04-27 19:00:30 +0100

    Add files via upload

--- scatterplot.py
+++ scatterplot.py
@@ -17,3 +17,23 @@ plt.show()
 sns.scatterplot(x="petal_length", y="petal_width", data=dataset, ax=axes[1], hue="species")
 #Display scatter plot
 plt.show()
+
+#Creating scatter plot of sepal length vs petal length
+sns.scatterplot(x="sepal_length", y="petal_length", data=dataset, ax=axes[0], hue="species")
+#Display scatter plot
+plt.show()
+
+#Creating scatter plot of sepal width vs petal width
+sns.scatterplot(x="sepal _width", y="petal_width", data=dataset, ax=axes[1], hue="species")
+#Display scatter plot
+plt.show()
+
+#Creating scatter plot of sepal length vs petal width
+sns.scatterplot(x="sepal_length", y="petal_width", data=dataset, ax=axes[1], hue="species")
+#Display scatter plot
+plt.show()
+
+#Creating a scatter plot of petal length vs sepal width
+sns.scatterplot(x="petal_length", y="sepal_width", data=dataset, ax=axes[1], hue="species")
+#Display scatter plot
+plt.show()
