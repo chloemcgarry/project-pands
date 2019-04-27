75fee91beba24f1823ef81d86cb0f2f4a5a6e293
chloemcgarry <47299403+chloemcgarry@users.noreply.github.com>
2019-04-27 17:08:37 +0100

    Update scatterplotsw.py

--- scatterplotsw.py
+++ scatterplotsw.py
@@ -1,7 +1,10 @@
 #Importing pandas library
 import pandas as pd 
+#Importing numpy library
+import numpy as np
 #Importing matplolib library
 import matplotlib.pyplot as plt 
+
 
 #Read csv file and call it data
 data = pd.read_csv('iris_data_set.csv')
